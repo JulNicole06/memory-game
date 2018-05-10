2190266e29125e50f71a480432cb2a5d0ac99735
Julia Fuhrman <JulNicole@gmail.com>
2018-05-10 16:40:12 -0400

    feat: Shuffle deck with button click

---
+++ README.md
@@ -0,0 +1,2 @@
+## Resources
+1. Implementation of Fisher-Yates Shuffle found [here](https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array)
--- app.js
+++ app.js
@@ -1,8 +1,39 @@
-const cards = document.querySelectorAll('.card');
+/* Animation to flip cards when clicked */
 
+const cards = document.querySelectorAll('.card');
 
 cards.forEach(function(card){
 	card.addEventListener("click", function(){
 		card.classList.toggle("flipped");
 	})
 })
+
+
+/* Randmize card display order with "Shuffle Deck" button */
+
+const shuffleButton = document.querySelector('#shuffle-button');
+let order = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+
+shuffleButton.addEventListener("click", function(){
+	let i = 0;
+	order = shuffle(order);
+	cards.forEach(function(card){
+		card.style.order = order[i];
+		i += 1;
+	})
+})
+
+/* Fisher-Yates Shuffle */
+
+function shuffle(array){
+	let currentIndex = array.length, temporaryValue, randomIndex;
+	while(0 !== currentIndex) {
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -=1;
+
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+	return array;
+}
